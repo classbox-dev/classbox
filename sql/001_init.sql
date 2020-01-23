81aa6de02eff19c4f75072dc3365e919502a9b74
Max Kuznetsov <maks.kuznetsov@gmail.com>
2020-01-23 22:15:59 +0300

    finalise sql schema

--- sql/001_init.sql
+++ sql/001_init.sql
@@ -1,91 +1,94 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE IF NOT EXISTS users
 (
     id    bigserial PRIMARY KEY,
     login text NOT NULL UNIQUE
 );
 
+-- -----------------------------------------------------------------------------
 
-DROP TABLE IF EXISTS tests;
+DROP TABLE IF EXISTS tests CASCADE;
 CREATE TABLE IF NOT EXISTS tests
 (
-    id         bigserial PRIMARY KEY,
-    name       text NOT NULL UNIQUE,
-    title      text NOT NULL,
-    topic      text NOT NULL,
-    is_deleted boolean DEFAULT FALSE
+    id          bigserial PRIMARY KEY,
+    name        text NOT NULL UNIQUE,
+    description text NOT NULL,
+    topic       text NOT NULL,
+    is_deleted  boolean DEFAULT FALSE
 );
 
+-- -----------------------------------------------------------------------------
 
-DROP TABLE IF EXISTS commits;
-CREATE TABLE IF NOT EXISTS commits
-(
-    id         bigserial PRIMARY KEY,
-    user_id    bigint REFERENCES users (id),
-    "commit"   text        NOT NULL,
-    updated_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
-CREATE INDEX commits__user_id ON commits (user_id);
-
-
-CREATE TYPE report_status_t AS ENUM (
-    'success',
-    'failure',
-    'exception'
-    );
-
-
-DROP TABLE IF EXISTS checks;
-CREATE TABLE IF NOT EXISTS checks
-(
-    id         bigserial PRIMARY KEY,
-    user_id    bigint REFERENCES users (id),
-    commit_id  bigint REFERENCES commits (id),
-    test_id    bigint REFERENCES tests (id)    DEFAULT NULL,
-    version_id bigint REFERENCES versions (id) DEFAULT NULL,
-    name       text            NOT NULL,
-    status     report_status_t NOT NULL,
-    output     text            NOT NULL
-);
-CREATE INDEX checks__user_id ON checks (user_id);
-CREATE INDEX checks__commit_id ON checks (commit_id);
-CREATE UNIQUE INDEX checks__user_commit ON checks (user_id, commit_id);
-
+DROP TYPE IF EXISTS run_status_t CASCADE;
 CREATE TYPE run_status_t AS ENUM (
     'success',
     'failure'
     );
 
-
-DROP TABLE IF EXISTS runs;
+DROP TABLE IF EXISTS runs CASCADE;
 CREATE TABLE IF NOT EXISTS runs
 (
     id     bigserial PRIMARY KEY,
     "hash" text         NOT NULL UNIQUE,
     status run_status_t NOT NULL,
-    perf   bigint       NOT NULL
+    output text         NOT NULL,
+    score  bigint       NOT NULL
 );
 
+-- -----------------------------------------------------------------------------
 
-DROP TABLE IF EXISTS versions;
-CREATE TABLE IF NOT EXISTS versions
-(
-    id     bigserial PRIMARY KEY,
-    tag    text NOT NULL,
-    active bool DEFAULT FALSE
-);
-
-
-DROP TABLE IF EXISTS baselines;
+DROP TABLE IF EXISTS baselines CASCADE;
 CREATE TABLE IF NOT EXISTS baselines
 (
-    id         bigserial PRIMARY KEY,
-    version_id bigint REFERENCES versions (id) DEFAULT NULL,
-    test_id    bigint REFERENCES tests (id)    DEFAULT NULL,
-    run_id     bigint REFERENCES tests (id)    DEFAULT NULL
+    id      bigserial PRIMARY KEY,
+    test_id bigint REFERENCES tests (id) DEFAULT NULL,
+    run_id  bigint REFERENCES runs (id)  DEFAULT NULL
 );
+CREATE INDEX commits__test_id ON baselines (test_id);
+CREATE INDEX commits__run_id ON baselines (run_id);
 
+-- -----------------------------------------------------------------------------
 
+DROP TABLE IF EXISTS commits CASCADE;
+CREATE TABLE IF NOT EXISTS commits
+(
+    id         bigserial PRIMARY KEY,
+    user_id    bigint REFERENCES users (id),
+    commit     text        NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+CREATE INDEX commits__user_id ON commits (user_id);
+CREATE UNIQUE INDEX commits__user_commit ON commits (user_id, commit);
+
+-- -----------------------------------------------------------------------------
+
+DROP TYPE IF EXISTS check_status_t CASCADE;
+CREATE TYPE check_status_t AS ENUM (
+    'success',
+    'failure',
+    'exception'
+    );
+
+DROP TABLE IF EXISTS checks CASCADE;
+CREATE TABLE IF NOT EXISTS checks
+(
+    id        bigserial PRIMARY KEY,
+    user_id   bigint REFERENCES users (id),
+    commit_id bigint REFERENCES commits (id),
+    test_id   bigint REFERENCES tests (id) DEFAULT NULL,
+    name      text           NOT NULL,
+    status    check_status_t NOT NULL,
+    output    text           NOT NULL,
+    is_cached boolean                      DEFAULT FALSE
+);
+CREATE INDEX checks__user_id ON checks (user_id);
+CREATE INDEX checks__commit_id ON checks (commit_id);
+CREATE INDEX checks__test_id ON checks (test_id);
+CREATE UNIQUE INDEX checks__user_commit ON checks (user_id, commit_id);
+
+-- -----------------------------------------------------------------------------
+
+DROP TYPE IF EXISTS task_status_t CASCADE;
 CREATE TYPE task_status_t AS ENUM (
     'enqueued',
     'executing',
@@ -93,8 +96,7 @@ CREATE TYPE task_status_t AS ENUM (
     'failure'
     );
 
-
-DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS tasks CASCADE;
 CREATE TABLE IF NOT EXISTS tasks
 (
     id          bigserial PRIMARY KEY,
@@ -105,5 +107,14 @@ CREATE TABLE IF NOT EXISTS tasks
     payload     jsonb       NOT NULL,
     status      task_status_t        DEFAULT 'enqueued'
 );
+CREATE INDEX "tasks__enqueued_idx"
+    ON tasks (topic, status) WHERE status = 'enqueued';
 
-CREATE INDEX "tasks__enqueued_idx" ON tasks (topic, status) WHERE status = 'enqueued';
+-- -----------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS versions CASCADE;
+CREATE TABLE IF NOT EXISTS versions
+(
+    id  bigserial PRIMARY KEY,
+    tag text NOT NULL
+);
