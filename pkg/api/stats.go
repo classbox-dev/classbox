1b4d0f0c0b5797351f891e26d1b1a8b949fb1308
Max Kuznetsov <maks.kuznetsov@gmail.com>
2020-02-05 03:24:13 +0300

    show all users in overall stats endpoint

--- pkg/api/stats.go
+++ pkg/api/stats.go
@@ -14,18 +14,21 @@ import (
 func (api *API) GetStats(w http.ResponseWriter, r *http.Request) {
 
 	rows, err := api.DB.Query(r.Context(), `
-	SELECT u.login, SUM(s.score) as score, COUNT(*) as count
-	FROM (
-		SELECT DISTINCT ON (ch.test_id, ci.user_id) ci.user_id, t.score, ch.status
-		FROM checks as ch
-			JOIN commits as ci ON (ci.id=ch.commit_id)
-			JOIN tests as t ON (t.id=ch.test_id)
-		WHERE ch.test_id IS NOT NULL AND t.is_deleted='f'
-		ORDER BY ch.test_id, ci.user_id, ch.id DESC
-	) as s
-	JOIN users as u ON (u.id=s.user_id)
-	WHERE s.status='success' GROUP BY u.id
-	ORDER BY score DESC;
+	SELECT u.login, COALESCE(st.score, 0) as score, COALESCE(st.count, 0) as count
+	FROM users as u LEFT JOIN (
+		SELECT u.id as user_id, SUM(s.score) as score, COUNT(*) as count
+		FROM (
+			SELECT DISTINCT ON (ch.test_id, ci.user_id) ci.user_id, t.score, ch.status
+			FROM checks as ch
+				JOIN commits as ci ON (ci.id=ch.commit_id)
+				JOIN tests as t ON (t.id=ch.test_id)
+			WHERE ch.test_id IS NOT NULL AND t.is_deleted='f'
+			ORDER BY ch.test_id, ci.user_id, ch.id DESC
+		) as s
+		JOIN users as u ON (u.id=s.user_id)
+		WHERE s.status='success' GROUP BY u.id
+	) as st ON (u.id=st.user_id)
+	ORDER BY score DESC, login;
 	`)
 	if err != nil {
 		E.Handle(w, r, err)
