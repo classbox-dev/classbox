97218df2a92d30ecbfba250bcd1cf4e8ab00c122
Max Kuznetsov <maks.kuznetsov@gmail.com>
2020-02-16 10:53:05 +0300

    pass docs url to quickstart page

--- pkg/web/quickstart.go
+++ pkg/web/quickstart.go
@@ -4,13 +4,18 @@ import (
 	"net/http"
 )
 
+type quickstartPage struct {
+	DocsURL string
+}
+
 func (web *Web) GetQuickstart(w http.ResponseWriter, r *http.Request) {
 	tpl, err := web.Templates.New("quickstart")
 	if err != nil {
 		web.HandleError(w, r, err)
 		return
 	}
-	if err := web.Render(w, tpl, nil); err != nil {
+	page := quickstartPage{DocsURL: web.DocsURL}
+	if err := web.Render(w, tpl, &page); err != nil {
 		web.HandleError(w, r, err)
 		return
 	}
--- web/templates/quickstart.md
+++ web/templates/quickstart.md
@@ -1,6 +1,6 @@
 {{define "title"}}Quickstart @ hsecode{{end -}}
 # Quickstart
 
-
+[docs]({{url .DocsURL "/cmp/index.html"}})
 
 * [Back to main page](..)
